6eac78425920f72e67f1f57ae57739d76cb55da0
Benjamin Müller <bennimueller94@yahoo.de>
2021-01-27 00:17:09 +0100

    Implemented more Plugins

--- analyzer/analyzer.py
+++ analyzer/analyzer.py
@@ -8,36 +8,16 @@ class Analyzer(ast.NodeVisitor):
         for node_type in registered_plugins:
             def func(instance, node):
                 for reg_plugin in registered_plugins[type(node).__name__]:
-                    registered_plugins[type(node).__name__][reg_plugin](instance, node)
+                    registered_plugins[type(node).__name__][reg_plugin]\
+                        (instance, node)
                 instance.generic_visit(node)
-            exec("Analyzer.visit_%s = func" % node_type)
+            setattr(Analyzer, "visit_%s" % node_type, func)
 
     def __init__(self, file):
         self.used_libraries = []
         self.vulnerabilities = set([])
         self.file = file
         self.create_visitor_functions()
-
-    def visit_Import(self, node):
-        """
-        Appends all used libraries e. g. requirements to self.used_libraries
-        :param node: The Import node
-        :return: None
-        """
-        for alias in node.names:
-            if alias not in self.used_libraries:
-                self.used_libraries.append(alias.name)
-        self.generic_visit(node)  # calls visit() on all children
-
-    def visit_ImportFrom(self, node):
-        """
-        Appends all used libraries e. g. requirements to self.used_libraries
-        :param node: The ImportFrom node
-        :return: None
-        """
-        if node.module not in self.used_libraries:
-            self.used_libraries.append(node.module)
-        self.generic_visit(node)
 
     def report(self):
         for vulnerability in self.vulnerabilities:
--- analyzer/analyzer_creator.py
+++
@@ -1,9 +0,0 @@
-from .analyzer import Analyzer
-from .settings_analyzer import SettingsAnalyzer
-
-
-def create_analyzer(file):
-    if file.endswith("settings.py"):
-        return SettingsAnalyzer(file)
-    else:
-        return Analyzer(file)
